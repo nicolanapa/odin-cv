2ccbf08b930e73ea7fd245325529e9098d6a1de6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-06-26 14:36:13 +0200

    Added the Function SingleForm() which will be used to build the remaining section of the Form without doubling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+function SingleForm(props) {
+	return (
+		<section>
+			<label htmlFor={props.id[0]}>{props.label[0]}</label>
+			<input type={props.type[0]} id={props.id[0]} />
+
+			<label htmlFor={props.id[1]}>{props.label[1]}</label>
+			<input type={props.type} id={props.id[1]} />
+
+			<label htmlFor={props.id[2]}>{props.label[2]}</label>
+			<input type={props.type[2]} id={props.id[2]} />
+		</section>
+	);
+}
+
+function Personal() {
+	const person = {
+		id: ["name", "email", "phone"],
+		label: ["Name ", "Email ", "Phone Number "],
+		type: ["text", "text", "tel"],
+	};
+}
+
+function Educational() {}
+
+function Practical() {}
+
 function Form() {
-	return <div></div>;
+	return (
+		<div>
+			<Personal />
+			<Educational />
+			<Practical />
+		</div>
+	);
 }
 
 export { Form };
