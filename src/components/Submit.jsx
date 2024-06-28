80a474616f7c00da6787369438a48945d196a8e6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-06-28 16:25:56 +0200

    Added a idList variable to each Submit Function to use it to take all the Input's Data

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 
 function PersonSubmit(e) {
+	const idList = {
+		id: ["name", "email", "phone"],
+	};
+
 	e.preventDefault();
 	console.log("Person");
 }
 
 function EducationSubmit(e) {
+	const idList = {
+		id: ["school", "study", "dateStudy"],
+	};
+
 	e.preventDefault();
 	console.log("Education");
 }
 
 function PracticSubmit(e) {
+	const idList = {
+		id: ["company", "position", "responsibilities", "dateWorkStart", "dateWorkEnd"],
+	};
+
 	e.preventDefault();
 	console.log("Practic");
 }
