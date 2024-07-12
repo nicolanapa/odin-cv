cc458b0f6b9497976c367f8e263f70e76f9dbedd
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-12 15:24:56 +0200

    Added a Container for the Cv to make it center and added the class 'cv'

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { ShowingPerson, ShowingEducation, ShowingPractic } from "./Showing.jsx";
-import "../styles/cv.css"
+import "../styles/cv.css";
 
 // Use state for input values variables
 
 function Cv(props) {
 	return (
-		<div>
-			<section className="person">
-				<ShowingPerson  {...props}/>
-			</section>
-			<section className="education">
-				<ShowingEducation  {...props}/>
-			</section>
-			<section className="practic">
-				<ShowingPractic  {...props}/>
-			</section>
+		<div className="cv-container">
+			<div className="cv">
+				<section className="person">
+					<ShowingPerson {...props} />
+				</section>
+				<section className="education">
+					<ShowingEducation {...props} />
+				</section>
+				<section className="practic">
+					<ShowingPractic {...props} />
+				</section>
+			</div>
 		</div>
 	);
 }
